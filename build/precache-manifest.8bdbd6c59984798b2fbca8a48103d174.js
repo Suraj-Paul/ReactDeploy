5b6aa9b7c02459125ff299febd53abf3544b1a9a
Suraj Paul <67500086+Suraj-Paul@users.noreply.github.com>
2020-09-21 16:31:11 +0530

    Add files via upload

---
+++ build/precache-manifest.8bdbd6c59984798b2fbca8a48103d174.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0350a1c413f83644d04e330981a90e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3cdad68aa9dff4b5cff",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "1ca19146cd97d03f9e65",
+    "url": "/static/css/main.d2dde9ca.chunk.css"
+  },
+  {
+    "revision": "e3cdad68aa9dff4b5cff",
+    "url": "/static/js/2.a7622b08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a7622b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca19146cd97d03f9e65",
+    "url": "/static/js/main.f89eb9f2.chunk.js"
+  },
+  {
+    "revision": "23eaed10320a62762f58",
+    "url": "/static/js/runtime-main.6847f719.js"
+  },
+  {
+    "revision": "3aed264f2ef785fe635cdc72d62706ad",
+    "url": "/static/media/foodWallpaper.3aed264f.png"
+  }
+]);
